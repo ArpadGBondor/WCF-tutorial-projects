c72f76f8b82909fdf4298a55c40b7d10537ccf0f
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-10 17:40:21 +0100

    12

---
+++ 12/EmployeeService/EmployeeService/IEmployeeService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using EntityLayer;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+
+    // Option 2: Apply ServiceKnownType attribute on the ServiceContract 
+    //[ServiceKnownType(typeof(PartTimeEmployeeEntity))]
+    //[ServiceKnownType(typeof(FullTimeEmployeeEntity))]
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        // Option 3: Apply ServiceKnownType attribute on specific OperationContracts
+        //[ServiceKnownType(typeof(PartTimeEmployeeEntity))]
+        //[ServiceKnownType(typeof(FullTimeEmployeeEntity))]
+        [OperationContract]
+        EmployeeEntity GetEmployee(int id);
+
+        // Operation contract change effects on existing clients:
+            // New parameter is inicialized to it's default value, if the client doesn't know about it. It does not break the existing client.
+            // Removed parameter does not break the client, the server just ignores the extra parameter. Possible data loss.
+            // Parameter or return type change does not break the client, if conversion is possible between the types. Exception occures otherwise.
+            // New operations: Client is not effected. It won't invoke operation it knows nothing about.
+            // Removed operation: Exception occures
+
+
+        //[ServiceKnownType(typeof(PartTimeEmployeeEntity))]
+        //[ServiceKnownType(typeof(FullTimeEmployeeEntity))]
+        [OperationContract]
+        void SaveEmployee(EmployeeEntity employee);
+
+    }
+}
---
+++ 12/EmployeeService/EntityLayer/EmployeeEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace EntityLayer
+{
+    // Option 1: Use KnownType attribute on the base type.
+    //[KnownType(typeof(FullTimeEmployeeEntity))]
+    //[KnownType(typeof(PartTimeEmployeeEntity))]
+    [DataContract(Namespace = "http://pragimtech.com/Employee")]
+    // Data contract change effects on existing clients:
+        // New non-required member: client wont send data, new member is set to its default value by the server
+        // New required member: exception occured because the client does not send data
+        // Removing non required member: Client continue to send data, the service ignores the data -> data loss
+        // Removing required data: Exception occures when the client recives from the service with missing values.
+        // Modify existing member data types: If the types are not compatible, exception occures.
+
+    [Table("Employees")]
+    public class EmployeeEntity
+    {
+        [DataMember(Name = "ID", Order = 1,IsRequired = true)]
+        [Key]
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Name { get; set; }
+
+        [DataMember(Order = 3)]
+        public string Gender { get; set; }
+
+        [DataMember(Order = 4)]
+        [Column(TypeName = "DateTime2")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataMember(Order = 5)]
+        public EmployeeType Type { get; set; }
+
+        // New member
+        [DataMember(Order = 6/*, IsRequired = true*/)]
+        public string City { get; set; }
+    }
+    public enum EmployeeType
+    {
+        FullTimeEmployee = 1,
+        PartTimeEmployee = 2
+    }
+}
