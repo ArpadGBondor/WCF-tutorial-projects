88f866cdf85b94b01e00304bd128721484a2eeb4
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-16 20:40:11 +0100

    15

---
+++ 15/CalculatorClient/CalculatorClient/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            int numerator = Convert.ToInt32(txtNumerator.Text);
+            int denominator = Convert.ToInt32(txtDenominator.Text);
+            CalculatorService.CalculatorServiceClient client =
+                new CalculatorService.CalculatorServiceClient();
+            lblResult.Text = client.Divide(numerator, denominator).ToString();
+        }
+    }
+}
---
+++ 15/CalculatorService/CalculatorService/ICalculatorService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace CalculatorService
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        int Divide(int Numerator, int Denominator);
+    }
+}
