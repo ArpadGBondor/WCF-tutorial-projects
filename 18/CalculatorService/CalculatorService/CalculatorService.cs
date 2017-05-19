09e1c33b97f3172c75480e45df75ed59dcde8dc7
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-19 14:08:45 +0100

    18

---
+++ 18/CalculatorService/CalculatorService/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CalculatorService
+{
+    // Option 2: enable includeExceptionDetailInFaults setting
+    // [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class CalculatorService : ICalculatorService
+    {
+        public int Divide(int Numerator, int Denominator)
+        {
+            if (Denominator == 0)
+                throw new FaultException("Denominator cannot be ZERO", new FaultCode("DivideByZeroFault"));
+            return Numerator / Denominator;
+        }
+    }
+}
---
+++ 18/CalculatorService/CalculatorServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorService;
+
+namespace CalculatorServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService.CalculatorService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
