c309872bdf1414b4e8ecc75e6fff94e826d26057
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-20 13:25:16 +0100

    21

---
+++ 21/HelloService/HelloService/HelloService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloService
+{
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string name)
+        {
+            return string.Format("Hello {0}!", name);
+        }
+    }
+}
---
+++ 21/HelloService/HelloServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
