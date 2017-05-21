a4a470ba28b090dd88cdd813b74dab719de9f1e0
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-21 11:02:25 +0100

    25

---
+++ 25/HelloService/HelloServiceWindowsHost/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+
+namespace HelloServiceWindowsHost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServiceHost host;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            host = new ServiceHost(typeof(HelloService.HelloService));
+            host.Open();
+            btnRun.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            lblResult0.Text = "Service started.";
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            host = new ServiceHost(typeof(HelloService.HelloService));
+            host.Open();
+            btnRun.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            lblResult0.Text = "Service started.";
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            host.Close();
+            btnRun.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            lblResult0.Text = "Service stopped.";
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            host.Close();
+            btnRun.IsEnabled = true;
+            btnStop.IsEnabled = false;
+        }
+    }
+}
---
+++ 25/WindowsClient/WindowsClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HelloService.HelloServiceClient client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new HelloService.HelloServiceClient();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (client.State == System.ServiceModel.CommunicationState.Faulted)
+                    client = new HelloService.HelloServiceClient();
+                lblResult.Text = client.GetMessage(txtName.Text);
+            }
+            catch (Exception exception)
+            {
+                lblResult.Text = exception.Message;
+            }
+        }
+    }
+}
