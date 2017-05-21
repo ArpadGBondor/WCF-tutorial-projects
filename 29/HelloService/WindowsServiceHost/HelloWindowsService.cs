adfd4f46d9b9751ef6dc3e5a7422f735bc429d11
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-21 22:47:36 +0100

    29

---
+++ 29/HelloService/WindowsServiceHost/HelloWindowsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WindowsServiceHost
+{
+    public partial class HelloWindowsService : ServiceBase
+    {
+        ServiceHost host;
+        public HelloWindowsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(HelloService.HelloService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+    }
+}
