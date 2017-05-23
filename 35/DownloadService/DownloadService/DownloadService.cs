e8261cb5f31134209a2c4db15d46c607130e62ca
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-23 23:58:53 +0100

    35

---
+++ 35/DownloadService/DownloadService/DownloadService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DownloadService
+{
+    public class DownloadService : IDownloadService
+    {
+        public File DownloadDocument()
+        {
+            File file = new File();
+            file.Content = System.IO.File.ReadAllBytes(@"C:\Users\Red\Documents\Visual Studio 2015\Projects\WCF-tutorial-projects\35\DownloadService\DownloadService\world.jpg");
+            file.Name = "world.jpg";
+
+            return file;
+        }
+    }
+}
