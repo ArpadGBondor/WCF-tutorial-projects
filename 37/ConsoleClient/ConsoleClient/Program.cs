d06e2a99f432624cd3110ca4b771a0020c79fcb7
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-24 09:47:31 +0100

    37

---
+++ 37/ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleService.SimpleServiceClient client = new SimpleService.SimpleServiceClient();
+
+            int number = client.IncrementNumber();
+            Console.WriteLine("Number after 1st call: {0}",number);
+
+            number = client.IncrementNumber();
+            Console.WriteLine("Number after 2nd call: {0}", number);
+
+            number = client.IncrementNumber();
+            Console.WriteLine("Number after 3rd call: {0}", number);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 37/SimpleService/SimpleService/SimpleService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleService
+{
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    //Number after 1st call: 1
+    //Number after 2nd call: 1
+    //Number after 3rd call: 1
+    // Implications of PerCall WCF service:
+    //  - Better memory usage as service objects are freed immediately after
+    //    method call returns
+    //  - Concurency is not an issue
+    //  - Application scalability is better
+    //  - State is not maintained between calls
+    //  - Performance could be an issue as there is overhead involved in 
+    //    reconstructing the service instance state on each and every method call
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class SimpleService : ISimpleService
+    {
+        private int _number;
+
+        public int IncrementNumber()
+        {
+            _number = _number + 1;
+            return _number;
+        }
+    }
+}
