5d3ce65fabc759ddd6695c015027d286a6dd35f0
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-05-25 09:34:52 +0100

    39

---
+++ 39/ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                SimpleService.SimpleServiceClient client = new SimpleService.SimpleServiceClient();
+
+                for (int j = 1; j <= 3; j++)
+                {
+                    int number = client.IncrementNumber();
+                    Console.WriteLine("Session: {0} Call: {1} Number: {2}",i,j, number);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
