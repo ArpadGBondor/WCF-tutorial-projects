d87f1e51a47e7bb2a142fc28d3271a3e39337d1f
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-06-06 18:00:27 +0100

    47 + Rename folders

---
+++ 47 - WCF security/WindowsClient/WindowsClient/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(13, 13);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(424, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Get Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(13, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(424, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(13, 70);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 117);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Windows Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ 47 - WCF security/WindowsClient/WindowsClient/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            HelloService.HelloServiceClient client =
+                new HelloService.HelloServiceClient();
+            lblResult.Text = client.GetMessage(txtName.Text);
+            MessageBox.Show(lblResult.Text);
+        }
+    }
+}
