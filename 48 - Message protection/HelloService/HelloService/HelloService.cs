58fbfce078ce1f7d859ecd0a4ff7969bc600d9f4
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-06-06 22:57:38 +0100

    48

---
+++ 48 - Message protection/HelloService/HelloService/HelloService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in both code and config file together.
+    public class HelloService : IHelloService
+    {
+        //public string GetMessage(string message)
+        //{
+        //    return $"Hello {message}!";
+        //}
+        public string GetMessageWithoutAnyProtection()
+        {
+            return "Message without signature and encryption";
+        }
+
+        public string GetSignedMessage()
+        {
+            return "Message with signature but without encryption";
+        }
+
+        public string GetSignedAndEncryptedMessage()
+        {
+            return "Message signed and encrypted";
+        }
+    }
+}
---
+++ 48 - Message protection/WindowsClient/WindowsClient/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public partial class Form1 : Form
+    {
+        HelloService.HelloServiceClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new HelloService.HelloServiceClient();
+        }
+
+        //private void btnSend_Click(object sender, EventArgs e)
+        //{
+        //    HelloService.HelloServiceClient client =
+        //        new HelloService.HelloServiceClient();
+        //    lblResult.Text = client.GetMessage(txtName.Text);
+        //    MessageBox.Show(lblResult.Text);
+        //}
+
+
+
+        private void btnGet1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(client.GetMessageWithoutAnyProtection());
+        }
+
+        private void btnGet2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(client.GetSignedMessage());
+        }
+
+        private void btnGet3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(client.GetSignedAndEncryptedMessage());
+        }
+    }
+}
