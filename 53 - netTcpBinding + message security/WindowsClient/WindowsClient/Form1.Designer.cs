5aabe7e89438022a26ef4cdbaf247336ad225943
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-06-08 15:50:59 +0100

    53

---
+++ 53 - netTcpBinding + message security/WindowsClient/WindowsClient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallService = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCallService
+            // 
+            this.btnCallService.Location = new System.Drawing.Point(13, 13);
+            this.btnCallService.Name = "btnCallService";
+            this.btnCallService.Size = new System.Drawing.Size(259, 23);
+            this.btnCallService.TabIndex = 0;
+            this.btnCallService.Text = "Call Service";
+            this.btnCallService.UseVisualStyleBackColor = true;
+            this.btnCallService.Click += new System.EventHandler(this.btnCallService_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCallService);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallService;
+    }
+}
+
---
+++ 53 - netTcpBinding + message security/WindowsClient/WindowsClient/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallService_Click(object sender, EventArgs e)
+        {
+            SimpleService.SimpleServiceClient client =
+                new SimpleService.SimpleServiceClient();
+            //MessageBox.Show(client.GetMessage("Windows username and password required here."));
+            //client.ClientCredentials.UserName.UserName = "******"; // Windows username required
+            //client.ClientCredentials.UserName.Password = "******"; // Windows password required
+            MessageBox.Show(client.GetMessage("Red"));
+        }
+    }
+}
